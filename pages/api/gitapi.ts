43a0f63d6ce3436fd436ee5146456d33dbba91e9
Eliot <eliot.meurillon@gmail.com>
2023-03-13 02:42:16 +0100

    test avec GraphQl

--- pages/api/gitapi.ts
+++ pages/api/gitapi.ts
@@ -1,19 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { GraphQLClient } from "graphql-request";
+
+interface Repo {
+  id: string;
+  name: string;
+}
+
+interface Response {
+  viewer: {
+    repositories: {
+      nodes: {
+        id: string;
+        name: string;
+      }[];
+    };
+  };
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const accessToken = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
-    const response = await fetch("https://api.github.com/user/repos", {
+    const graphQLClient = new GraphQLClient("https://api.github.com/graphql", {
       headers: {
-        Authorization: `token ${accessToken}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
-    console.log(accessToken);
-    res.status(200).json(data);
+
+    const query = `
+      query {
+        viewer {
+          repositories(first: 10) {
+            nodes {
+              id
+              name
+            }
+          }
+        }
+      }
+    `;
+
+    const data: Response = await graphQLClient.request(query);
+
+    const repos: Repo[] = data.viewer.repositories.nodes.map((repo) => ({
+      id: repo.id,
+      name: repo.name,
+    }));
+
+    res.status(200).json(repos);
   } catch (error) {
     console.error(error);
     res
