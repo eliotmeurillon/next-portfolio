8fb48598c88d7460c3f555132240ab9aa2e2550d
Eliot <eliot.meurillon@gmail.com>
2023-03-13 14:41:05 +0100

    add framer motion and build a carousel

--- pages/fetch.tsx
+++ pages/fetch.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 
 interface Repo {
   id: string;
@@ -22,6 +23,9 @@ interface Repo {
 export default function Fetch() {
   const [repos, setRepos] = useState<Repo[]>([]);
 
+  const [width, setWidth] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     async function fetchRepos() {
       const response = await fetch("/api/gitapi");
@@ -31,30 +35,50 @@ export default function Fetch() {
     fetchRepos();
   }, []);
 
+  useEffect(() => {
+    if (carousel.current) {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }
+  }, [repos]);
+
   return (
     <div className="bg-gray-200">
       <h1 className="text-3xl font-bold underline">Mes Repositories GitHub</h1>
-      <ul className="">
-        {repos.map((repo) => (
-          <li className="rounded-lg shadow-lg m-3 bg-white" key={repo.id}>
-            <h2 className="text-xl">{repo.name}</h2>
-            <p>{repo.description}</p>
-            <a className="text-base" href={repo.url}>
-              Visit Repo
-            </a>
-            <ul>
-              {repo.repositoryTopics?.nodes?.map((topic) => (
-                <li
-                  className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"
-                  key={topic.topic.name}
-                >
-                  {topic.topic.name}
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      <motion.div ref={carousel} className="carousel overflow-hidden">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel flex"
+        >
+          {repos.map((repo) => (
+            <motion.div
+              className="item rounded-lg shadow-lg m-3 bg-white"
+              key={repo.id}
+            >
+              <h2 className="text-xl">{repo.name}</h2>
+              <p>{repo.description}</p>
+              <a
+                className="text-base"
+                target="_blank"
+                rel="noopener"
+                href={repo.url}
+              >
+                Visit Repo
+              </a>
+              <ul>
+                {repo.repositoryTopics?.nodes?.map((topic) => (
+                  <li
+                    className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"
+                    key={topic.topic.name}
+                  >
+                    {topic.topic.name}
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
