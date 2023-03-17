67c8074e7005a9a3e54eadae9d6a64d456c959a3
Eliot <eliot.meurillon@gmail.com>
2023-03-17 01:42:49 +0100

    whileOver

--- components/Switcher.jsx
+++ components/Switcher.jsx
@@ -17,7 +17,9 @@ export default function Switcher() {
 
   return (
     <motion.div
+      whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
       className="bg-green-500 rounded-full p-1"
     >
       {darkSide ? (
