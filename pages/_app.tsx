bac16d38ce1cb37105f2c1a4f3563bfcc5929436
Eliot <eliot.meurillon@gmail.com>
2023-03-19 19:56:40 +0100

    change as string

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import type { AppProps } from "next/app";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 
 const poppins = Poppins({
