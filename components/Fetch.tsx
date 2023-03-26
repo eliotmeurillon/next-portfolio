8c69ab84c3f8128f68675da70153bcc8512747e2
Eliot <eliot.meurillon@gmail.com>
2023-03-26 01:02:18 +0100

    return

--- components/Fetch.tsx
+++ components/Fetch.tsx
@@ -1,5 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 interface Repo {
   id: string;
@@ -7,8 +7,10 @@ interface Repo {
   description?: string;
   url: string;
   updatedAt: string;
-  primary_language_name?: string;
-  primary_language_color?: string;
+  primaryLanguage?: {
+    name: string;
+    color: string;
+  };
   repositoryTopics?: {
     nodes: {
       topic: {
@@ -19,15 +21,24 @@ interface Repo {
   illu_url: string;
 }
 
-interface Props {
-  repos: Repo[];
-}
-
-export default function Fetch({ repos }: Props) {
+export default function Fetch({ supabase }: any) {
+  const [repos, setRepos] = useState<Repo[]>([]);
   const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
   const [selectedTopic, setSelectedTopic] = useState<string>("");
 
-  function filterRepos() {
+  useEffect(() => {
+    async function getRepos() {
+      const { data: repos, error } = await supabase
+        .from("repos")
+        .select("*")
+        .order("name", { ascending: true });
+      if (error) console.error(error);
+      if (repos) setRepos(repos);
+    }
+    getRepos();
+  }, []);
+
+  useEffect(() => {
     if (selectedTopic === "") {
       setFilteredRepos(repos);
     } else {
@@ -38,86 +49,50 @@ export default function Fetch({ repos }: Props) {
       );
       setFilteredRepos(filtered);
     }
-  }
+  }, [repos, selectedTopic]);
 
   return (
     <div>
       <div className="carousel overflow-hidden cursor-grabbing">
         <div className="inner-carousel flex flex-col">
           <div>
-            <button
-              onClick={() => {
-                setSelectedTopic("react");
-                filterRepos();
-              }}
-            >
-              React
-            </button>
-            <button
-              onClick={() => {
-                setSelectedTopic("typescript");
-                filterRepos();
-              }}
-            >
+            <button onClick={() => setSelectedTopic("react")}>React</button>
+            <button onClick={() => setSelectedTopic("typescript")}>
               TypeScript
             </button>
-            <button
-              onClick={() => {
-                setSelectedTopic("");
-                filterRepos();
-              }}
-            >
-              Clear Filter
-            </button>
+            <button onClick={() => setSelectedTopic("")}>Clear Filter</button>
           </div>
-          {filteredRepos.map((repo) => (
-            <div
-              className="item rounded-lg shadow-lg p-3 m-3 min-w-fit bg-white"
-              key={repo.id}
-            >
-              <h2 className="text-xl">{repo.name}</h2>
-              <p>{repo.description}</p>
-              <a
-                className="text-base"
-                target="_blank"
-                rel="noopener"
-                href={repo.url}
+          {filteredRepos &&
+            filteredRepos.map((repo) => (
+              <div
+                className="item rounded-lg shadow-lg p-3 m-3 min-w-fit bg-white"
+                key={repo.id}
               >
-                Visit Repo
-              </a>
-              <img src={repo.illu_url} alt="testillu" />
-              <ul>
-                {repo.repositoryTopics?.nodes?.map((topic) => (
-                  <li
-                    className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"
-                    key={topic.topic.name}
-                  >
-                    {topic.topic.name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+                <h2 className="text-xl">{repo.name}</h2>
+                <p>{repo.description}</p>
+                <a
+                  className="text-base"
+                  target="_blank"
+                  rel="noopener"
+                  href={repo.url}
+                >
+                  Visit Repo
+                </a>
+                <img src={repo.illu_url} alt="testillu" />
+                <ul>
+                  {repo.repositoryTopics?.nodes?.map((topic) => (
+                    <li
+                      className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"
+                      key={topic.topic.name}
+                    >
+                      {topic.topic.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
         </div>
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  console.log("getServerSideProps");
-  const supabase = createClient(
-    process.env.SUPABASE_URL as string,
-    process.env.SUPABASE_ANON_KEY as string
-  );
-  const { data: repos, error } = await supabase
-    .from("repos")
-    .select("*")
-    .order("name", { ascending: true });
-  if (error) console.error(error);
-  return {
-    props: {
-      repos: repos || [],
-    },
-  };
 }
